c7e4daf12c792378f859893db33b94a420a6c739
ali-consciousroad <ali_dindar@live.be>
2025-03-31 15:21:25 +0100

    Add comments for better readability

--- src/App.js
+++ src/App.js
@@ -14,10 +14,15 @@ export default function App() {
   function spendGiftCard() {
     // Update the giftCard state to reflect that the coupon has been used.
     // The functional form of setGiftCard ensures we work with the latest state (prevState).
-    // The spread operator (...prevState) preserves existing properties while updating specific fields.
+    /* Using ...prevState in a new object will copy all the properties into the new object.
+       If additional properties are specified after ...prevState, 
+       they will overwrite the corresponding properties from prevState.*/
+    /* prevState is a name that you give to the argument passed to seState callback fct. 
+       What it holds is the value of state before the setState was triggered by React.  
+   */
     setGiftCard(prevState => {
       return {
-        ...prevState,
+        ...prevState, // Immutability principle respected: Leaves the original state untouched by creating a new object. 
         text: "Your coupon has been used.", // Update the displayed message.
         valid: false, // Mark the gift card as no longer valid.
         instructions: "Please visit our restaurant to renew your gift card." // Provide renewal instructions.
